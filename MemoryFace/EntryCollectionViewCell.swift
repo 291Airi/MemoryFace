fee72f3da4704eac01ee6125349c523bd31daf7c
Fukui Airi <loveairi291@gmail.com>
2019-10-26 19:34:54 +0900

    ・デザイン ・フォトライブラリにアクセス

---
+++ MemoryFace/EntryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  EntryCollectionViewCell.swift
+//  MemoryFaceTests
+//
+//  Created by 福井　愛梨 on 2019/10/26.
+//  Copyright © 2019 福井　愛梨. All rights reserved.
+//
+
+import UIKit
+
+class EntryCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ MemoryFace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoryFace
+//
+//  Created by 福井　愛梨 on 2019/10/03.
+//  Copyright © 2019 福井　愛梨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
